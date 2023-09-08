1a8a264122ea677a74195f0bb788f9e0ed408e9e
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-09-08 03:33:26 -0700

    python done

--- calculation.py
+++ calculation.py
@@ -1,1 +1,36 @@
+import mysql.connector
 
+# Ühendus andmebaasiga
+connection = mysql.connector.connect(
+    host="localhost",
+    user="kasutaja",
+    password="parool",
+    database="palgakalkulaator"
+)
+
+cursor = connection.cursor()
+
+# Funktsioon uue palga lisamiseks andmebaasi
+def lisa_palk(nimi, amet, palk):
+    sql = "INSERT INTO palk (nimi, amet, palk) VALUES (%s, %s, %s)"
+    val = (nimi, amet, palk)
+    cursor.execute(sql, val)
+    connection.commit()
+
+# Rakenduse põhiosa
+if __name__ == "__main__":
+    print("Tere tulemast palgakalkulaatorisse!")
+    while True:
+        valik = input("Kas soovite lisada uut palka (lisa) või kalkuleerida palkade summat (kalkuleeri)? ")
+        if valik == "lisa":
+            nimi = input("Sisestage töötaja nimi: ")
+            amet = input("Sisestage töötaja amet: ")
+            palk = float(input("Sisestage töötaja palk: "))
+            lisa_palk(nimi, amet, palk)
+            print(f"{nimi} palk on lisatud andmebaasi.")
+        elif valik == "kalkuleeri":
+            cursor.execute("SELECT SUM(palk) FROM palk")
+            summa = cursor.fetchone()[0]
+            print(f"Palkade summa on: {summa}")
+        else:
+            print("Vale valik. Palun sisestage 'lisa' või 'kalkuleeri'.")
