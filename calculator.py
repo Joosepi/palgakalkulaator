bf719c1b453fee8d9a96e543a2c355d25b4e95cc
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-09-29 00:58:49 -0700

    removedstuf

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def main():
-    # Ask user for employee name
+    
     employee_name = input('Enter employee\'s name: ')
 
-    # Ask user for number of hours worked
+
     hours_worked = float(input('Enter amount of hours worked: '))
 
-    # Ask user for employee's pay rate
+    
     pay_rate = float(input('Enter the employee\'s pay rate: '))
     overtime_pay_rate = pay_rate * 1.5
 
-    # Calculate regular and overtime pay based on actual hours worked
+    
     if hours_worked > 40:
         overtime_hours = hours_worked - 40
         regular_hours = 40
@@ -20,15 +20,15 @@ def main():
     regular_pay = regular_hours * pay_rate
     overtime_pay = overtime_hours * overtime_pay_rate
 
-    # Calculate bruto pay and deductions for neto pay
+    
     total_bruto_pay = regular_pay + overtime_pay
 
-    # You can change the tax rate and other deductions as needed
-    tax_rate = 0.20  # Example tax rate (you can adjust this)
+
+    tax_rate = 0.20  
     total_deductions = total_bruto_pay * tax_rate
     total_neto_pay = total_bruto_pay - total_deductions
 
-    # Display employee details, bruto pay, and neto pay
+    
     print('+' + '-'*90 + '+')
     print('|{:<40} {:<14}|'.format('Employee name:', employee_name))
     print('|{:<40} {:<14}|'.format('Hours Worked:', '{:.2f}'.format(hours_worked)))
