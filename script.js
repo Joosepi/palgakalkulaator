51bb9e7b1093f81573f64878754eb2501e1eeeb7
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-10-04 00:41:03 -0700

    changes

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function calculateSalary() {
-   
     const employeeName = document.getElementById('employeeName').value;
     const hoursWorked = parseFloat(document.getElementById('hoursWorked').value);
     const payRate = parseFloat(document.getElementById('payRate').value);
+    const taxRate = parseFloat(document.getElementById('taxRate').value); 
 
-    
     const overtimePayRate = payRate * 1.5;
     const regularHours = hoursWorked > 40 ? 40 : hoursWorked;
     const overtimeHours = hoursWorked > 40 ? hoursWorked - 40 : 0;
     const regularPay = regularHours * payRate;
     const overtimePay = overtimeHours * overtimePayRate;
     const totalBrutoPay = regularPay + overtimePay;
-    const taxRate = 0.20;
-    const totalDeductions = totalBrutoPay * taxRate;
+
+    
+    const totalDeductions = totalBrutoPay * (taxRate / 100); 
+
     const totalNetoPay = totalBrutoPay - totalDeductions;
 
-   
     const resultHTML = `
         <p><strong>Employee Name:</strong> ${employeeName}</p>
         <p><strong>Hours Worked:</strong> ${hoursWorked.toFixed(2)}</p>
@@ -29,7 +29,6 @@ function calculateSalary() {
         <p><strong>Neto Pay:</strong> $${totalNetoPay.toFixed(2)}</p>
     `;
 
-   
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = resultHTML;
 }
